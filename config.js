048da502c642d2f3fa8648a85052934b01b8e19e
kavinduh659 <kavinduh659@gmail.com>
2024-08-17 21:23:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID,1VJFgIbS#EiWZnUDq92X-fcYu2PB_Ccbuge6h9HRxAOV__kSUdJM
 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/397000a07a1deb7fad9c2.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO,IM GHOST-MD I AM ALIVE NOW!",
